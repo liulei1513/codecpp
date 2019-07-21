16cd40faa7ff6c2060b648a978cb839c2a9cce2e
liulei <liulei1513@outlook.com>
2019-07-22 05:51:49 +0800

    update

--- test.c
+++ test.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 
+int func(int a, int b);
+
 int main(int argc, char const *argv[])
 {
     /* code */
-    printf("123");
+    printf("hello world!\n");
+    int c = func(3, 4);
+    printf("end:%d\n", c);
     return 0;
 }
+
+
+int func(int a, int b){
+    int result = a + b;
+    return result;
+}
+
